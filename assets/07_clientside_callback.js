121494a2c9a3cef99b3150bc212bcdcc4c2f49bf
Joern Ploennigs <Joern.Ploennigs@ie.ibm.com>
2023-11-07 15:32:15 +0100

    Update 07_clientside_callback.js

--- assets/07_clientside_callback.js
+++ assets/07_clientside_callback.js
@@ -1,9 +1,5 @@
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
-        test_client_side: function () {
-            console.log("called it ");
-            data = { style: "Ancient Egyptian", epoche: Math.random(), location: { x: 20, y: 30 } }
-            return data;
-        }
+        test_client_side: ()=> {return ticktick()}
     }
 });
