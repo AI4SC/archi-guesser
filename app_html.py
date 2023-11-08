d5d355e9c586b83800dee14e943fc301f5b71f7d
Joern Ploennigs <Joern.Ploennigs@ie.ibm.com>
2023-11-08 16:55:40 +0100

    Update app_html.py

--- app_html.py
+++ app_html.py
@@ -9,7 +9,7 @@ import random
 import dash_leaflet as dl
 from app_map import *
 
-mask = False
+mask = True
 
 archig_image = Image.open("archiguesser_logo.png")
 style_image = Image.open("styles_crop.png")
@@ -83,35 +83,45 @@ def init_webpage():
                 dbc.Col(
                     [
                         dbc.Label("STYLE"),
-                        html.Section(
-                            [
-                                dbc.Row(
-                                    [
-                                        dbc.Col(
-                                            dbc.Button(
-                                                [
-                                                    html.Img(src=img, alt=n),
-                                                    html.Br(),
-                                                    html.P(n, style={}),
-                                                ],
-                                                color=None,#"dark"
-                                                name=n,
-                                                style={
-                                                    "width": "140px",
-                                                    "height": "160px",
-                                                    "border-radius": "0px",
-                                                    #"background-color":"black"
-                                                },
-                                                id={
-                                                    "type": "style-selection",
-                                                    "index": n,
-                                                },
+                        html.Div([
+                            html.Section(
+                                [
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(
+                                                dbc.Button(
+                                                    [
+                                                        html.Img(src=img, alt=n),
+                                                        html.Br(),
+                                                        html.P(n, style={}),
+                                                    ],
+                                                    color=None,  #"dark"
+                                                    name=n,
+                                                    style={
+                                                        "width": "140px",
+                                                        "height": "160px",
+                                                        "border-radius": "0px",
+                                                        #"background-color":"black"
+                                                    },
+                                                    id={
+                                                        "type": "style-selection",
+                                                        "index": n,
+                                                    },
+                                                )
                                             )
-                                        )
-                                        for n, img in style_img.items()
-                                    ],
-                                    className="no-gutters no-padders",
-                                ),
+                                            for n, img in style_img.items()
+                                        ],
+                                        className="no-gutters no-padders",
+                                    ),
+                                ],
+                                style={
+                                    "width": "100%",
+                                    "height": "320px",
+                                    "overflow": "scroll",
+                                    "position": "static",
+                                },
+                                className="scrollable",
+                            ),
                                 html.Div(
                                     [
                                         html.H3(
@@ -126,114 +136,105 @@ def init_webpage():
                                         )
                                     ],
                                     style={
-                                        "width": "50%",
-                                        "height": "320px",
-                                        "position": "fixed",
-                                        "top": "100px",
-                                        "left": "50%",
+                                        "top": "0px",
+                                        "left": "0px",
+                                        "width": "100%",
+                                        "height": "100%",
+                                        "position": "absolute",
                                         "z-index": "10",
                                         "background-color": "rgba(34, 34, 34, 0.8)",
                                     },
-                                    hidden=True,  # mask,
+                                    hidden=~mask,
                                     id="style-mask",
-                                ),
-                            ],
-                            style={
-                                "width": "100%",
-                                "height": "320px",
-                                "overflow": "scroll",
-                                "position": "static",
-                            },
-                            className="scrollable",
-                        ),
+                                )
+                        ],style={"position": "relative"}),
                         dbc.Label("LOCATION"),
-                        # dcc.Graph(
-                        #  figure=mapfig, id='map', style={
-                        #    'width': '100%',
-                        #    'height': '30%'
-                        #  }),
-                        dl.Map(
-                            children=layers,  # url=bwtileurl
-                            id="map",
-                            style={
-                                "width": "100%",
-                                "height": "40vh",
-                                "margin": "auto",
-                                "z-index": "10",
-                                "display": "block",
-                            },
-                            maxZoom=4,
-                            zoom=1,
-                        ),
-                        html.Div(
-                            [
-                                html.H3(
-                                    "Please place the 3D object on the map",
-                                    style={
-                                        "text-align": "center",
-                                        "width": "100%",
-                                        "height": "50px",
-                                        "position": "absolute",
-                                        "top": "100px",
-                                    },
-                                )
-                            ],
-                            style={
-                                "width": "50%",
-                                "height": "460px",
-                                "position": "fixed",
-                                "top": "455px",
-                                "left": "50%",
-                                "z-index": "20",
-                                "background-color": "rgba(34, 34, 34, 0.8)",
-                            },
-                            hidden=mask,
-                            id="map-mask",
-                        ),
+                        html.Div([
+                            dl.Map(
+                                children=layers,  # url=bwtileurl
+                                id="map",
+                                style={
+                                    "width": "100%",
+                                    "height": "40vh",
+                                    "margin": "auto",
+                                    "z-index": "10",
+                                    "display": "block",
+                                },
+                                maxZoom=4,
+                                zoom=1,
+                            ),
+                            html.Div(
+                                [
+                                    html.H3(
+                                        "Please place the 3D object on the map",
+                                        style={
+                                            "text-align": "center",
+                                            "width": "100%",
+                                            "height": "50px",
+                                            "position": "absolute",
+                                            "top": "100px",
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "top": "0px",
+                                    "left": "0px",
+                                    "width": "100%",
+                                    "height": "100%",
+                                    "position": "absolute",
+                                    "z-index": "20",
+                                    "background-color": "rgba(34, 34, 34, 0.8)",
+                                },
+                                hidden=~mask,
+                                id="map-mask",
+                            ),
+                        ],style={"position": "relative"}),
                         dbc.Label("EPOCHE"),
-                        dcc.Slider(
-                            0,
-                            2025,
-                            step=5,
-                            marks={
-                                500: "Ancient and Classical Periods",
-                                1500: "Medieval Period",
-                                1700: "Renaissance",
-                                1800: "18th",
-                                1900: "19th",
-                                2000: "20th",
-                                2025: "21th",
-                            },
-                            value=1200,
-                            included=False,
-                            tooltip={"placement": "bottom", "always_visible": True},
-                            id="epoche",
-                        ),
-                        html.Div(
-                            [
-                                html.H3(
-                                    "Please select an epoche",
-                                    style={
-                                        "text-align": "center",
-                                        "width": "100%",
-                                        "height": "50px",
-                                        "position": "absolute",
-                                        "top": "10px",
-                                    },
-                                )
-                            ],
-                            style={
-                                "width": "50%",
-                                "height": "50px",
-                                "position": "fixed",
-                                "top": "920px",
-                                "left": "50%",
-                                "z-index": "20",
-                                "background-color": "rgba(34, 34, 34, 0.8)",
-                            },
-                            hidden=mask,
-                            id="epoche-mask",
-                        ),
+                        html.Div([
+                            dcc.Slider(
+                                0,
+                                2025,
+                                step=5,
+                                marks={
+                                    500: "Ancient and Classical Periods",
+                                    1500: "Medieval Period",
+                                    1700: "Renaissance",
+                                    1800: "18th",
+                                    1900: "19th",
+                                    2000: "20th",
+                                    2025: "21th",
+                                },
+                                value=1200,
+                                included=False,
+                                tooltip={"placement": "bottom", "always_visible": True},
+                                id="epoche",
+                            ),
+                            html.Div(
+                                [
+                                    html.H3(
+                                        "Please select an epoche",
+                                        style={
+                                            "text-align": "center",
+                                            "width": "100%",
+                                            "height": "50px",
+                                            "position": "absolute",
+                                            "top": "10px",
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "top": "0px",
+                                    "left": "0px",
+                                    "width": "100%",
+                                    "height": "100%",
+                                    "position": "absolute",
+                                    "z-index": "20",
+                                    "background-color": "rgba(34, 34, 34, 0.8)",
+                                },
+                                hidden=~mask,
+                                id="epoche-mask",
+                            ),
+                        ],style={"position": "relative"}),
                         dbc.Row(
                             [
                                 dbc.Col(
