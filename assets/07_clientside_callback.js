a93718c8d69c2e538726cd3ed1bd20e0a784861b
Joern Ploennigs <Joern.Ploennigs@ie.ibm.com>
2023-11-10 11:28:42 +0100

    Update 07_clientside_callback.js

--- assets/07_clientside_callback.js
+++ assets/07_clientside_callback.js
@@ -1,4 +1,4 @@
-var DEBUG_MODE=true, DEBUG_OBJ={"err":[], "status":"STOP", 'tick':0};
+var DEBUG_MODE=true, DEBUG_OBJ={"err":[], "state":"STOP", 'tick':0};
 
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
@@ -6,10 +6,10 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             if (DEBUG_MODE) {
                 DEBUG_OBJ['tick']+=1
                 if (DEBUG_OBJ['tick']>20) {
-                    if (DEBUG_OBJ['status']=='STOP') {
-                        DEBUG_OBJ={"err":[], "status":"GO", "lat":Math.random()*360-180, "lon":Math.random()*180-90, 'obj':20+Math.trunc(10*Math.random()), 'year':Math.trunc(2500*Math.random()-500), 'tick':0}
+                    if (DEBUG_OBJ['state']=='STOP') {
+                        DEBUG_OBJ={"err":[], "state":"GO", "lat":Math.random()*360-180, "lon":Math.random()*180-90, 'obj':20+Math.trunc(10*Math.random()), 'year':Math.trunc(2500*Math.random()-500), 'tick':0}
                     } else {
-                        DEBUG_OBJ={"err":[], "status":"STOP", 'tick':0}
+                        DEBUG_OBJ={"err":[], "state":"STOP", 'tick':0}
                     }
                     console.log(DEBUG_OBJ)
                 }
